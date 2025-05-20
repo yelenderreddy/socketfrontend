9bc24ef49bf47c1bc9e358ee37c5b29308937b36
yelenderreddy <148934745+yelenderreddy@users.noreply.github.com>
2025-05-20 10:28:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:4000"); // Connect to server
+const socket = io("https://socketbackend-production-0ab5.up.railway.app"); // Connect to server
 
 function App() {
   const [message, setMessage] = useState("");
